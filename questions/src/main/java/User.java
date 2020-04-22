baaacfe8a6f9d78d008cc2ca8cd32567729ba03b
xpc2018 <21725151@zju.edu.cn>
2020-04-22 23:49:57 +0800

    新建项目，新建User类

---
+++ questions/src/main/java/User.java
@@ -0,0 +1,31 @@
+package main.java;
+
+public class User {
+    private String name;
+    private int age;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
